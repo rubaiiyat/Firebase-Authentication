0749974843b1c91951d6c3980991958b58e92ae1
rubaiiyat <abirr1516@gmail.com>
2025-01-15 23:42:57 +0600

    add error message

--- 02-Simple-Form/src/Components/Register/Register.jsx
+++ 02-Simple-Form/src/Components/Register/Register.jsx
@@ -1,16 +1,34 @@
-import React from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";
+import auth from "../Firebase/Firebase";
 
 const Register = () => {
+  const [registerError, setRegisterError] = useState("");
+  const [registerSuccess, setRegisterSuccess] = useState("");
+
   const handleRegisterForm = (e) => {
     e.preventDefault();
     const email = e.target.email.value;
     const password = e.target.password.value;
-    console.log("working");
     console.log(email, password);
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result.user);
+        setRegisterError("");
+
+        setRegisterSuccess("Account Created Successfully");
+      })
+      .catch((error) => {
+        setRegisterError(error.message);
+        setRegisterSuccess("");
+      });
   };
 
   return (
     <div className="w-96 mx-auto">
+      <h1 className="text-red-400 mt-2 mb-2">{registerError}</h1>
+      <h1 className="text-green-400 mt-2 mb-2">{registerSuccess}</h1>
       <form onSubmit={handleRegisterForm}>
         <input
           type="email"
