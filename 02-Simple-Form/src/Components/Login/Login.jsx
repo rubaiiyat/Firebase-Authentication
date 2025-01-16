7a985721aa8aeff78986fede13ea67114985563d
rubaiiyat <abirr1516@gmail.com>
2025-01-16 21:29:11 +0600

    handle forgot password

--- 02-Simple-Form/src/Components/Login/Login.jsx
+++ 02-Simple-Form/src/Components/Login/Login.jsx
@@ -1,5 +1,8 @@
-import { signInWithEmailAndPassword } from "firebase/auth";
-import React, { useState } from "react";
+import {
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import React, { useRef, useState } from "react";
 import auth from "../Firebase/Firebase";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 
@@ -7,6 +10,7 @@ const Login = () => {
   const [loginError, setLoginError] = useState("");
   const [loginSuccess, setLoginSuccess] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+  const emailRef = useRef(null);
 
   const handleLoginForm = (e) => {
     e.preventDefault();
@@ -27,6 +31,31 @@ const Login = () => {
         setLoginError("Incorrect username or password");
       });
   };
+
+  const handleForgotPassword = (e) => {
+    e.preventDefault();
+    const email = emailRef.current.value;
+
+    setLoginError("");
+    setLoginSuccess("");
+
+    if (!email) {
+      setLoginError("Enter the valid email");
+      return;
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+      setLoginError("Enter the valid email");
+      return;
+    }
+
+    sendPasswordResetEmail(auth, email)
+      .then((result) => {
+        console.log("Please check your email");
+        setLoginSuccess("Please check your email inbox");
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
   return (
     <div className="w-96 mx-auto">
       <h1 className="text-red-400 mt-2 mb-2">{loginError}</h1>
@@ -36,6 +65,7 @@ const Login = () => {
           type="email"
           placeholder="Enter your email"
           name="email"
+          ref={emailRef}
           className="input input-bordered input-primary w-full mb-3"
         />
         <div className="relative">
@@ -53,7 +83,11 @@ const Login = () => {
             {showPassword ? <FaEyeSlash /> : <FaEye />}
           </span>
         </div>
-
+        <div className="mt-2 text-start text-white">
+          <a href="" onClick={handleForgotPassword} className="">
+            Forgot Password?
+          </a>
+        </div>
         <button className="btn btn-primary mt-3 w-full text-xl text-white">
           Login
         </button>
