974427e9e4e802c2427678351b604fbf95383f2b
rubaiiyat <abirr1516@gmail.com>
2025-01-17 22:51:12 +0600

    make sign in and sign up form and change header color

--- 03-Auth-Integration/src/Components/Signin/Signin.jsx
+++ 03-Auth-Integration/src/Components/Signin/Signin.jsx
@@ -3,7 +3,47 @@ import React from "react";
 const Signin = () => {
   return (
     <div>
-      <h1>Please Sign in</h1>
+      <h1 className="text-3xl text-white mt-10 font-bold">Please Sign in</h1>
+      <div className="hero">
+        <div className="card bg-base-300 w-full max-w-sm shrink-0 shadow-2xl mt-5">
+          <form className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email"
+                name="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                name="password"
+                className="input input-bordered"
+                required
+              />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary text-white text-xl">
+                Sign in
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
--- 03-Auth-Integration/src/Components/Signup/Signup.jsx
+++ 03-Auth-Integration/src/Components/Signup/Signup.jsx
@@ -2,8 +2,60 @@ import React from "react";
 
 const Signup = () => {
   return (
-    <div>
-      <h1>Please Signup</h1>
+    <div className="">
+      <h1 className="text-3xl text-white mt-10 font-bold">Please Sign up</h1>
+      <div className="hero">
+        <div className="card bg-base-300 w-full max-w-sm shrink-0 shadow-2xl mt-5">
+          <form className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Full Name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="full name"
+                name="name"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email"
+                name="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                name="password"
+                className="input input-bordered"
+                required
+              />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary text-white text-xl">
+                Sign up
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
