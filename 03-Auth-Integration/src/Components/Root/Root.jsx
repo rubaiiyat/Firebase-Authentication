8713a39a0e62ad66542a8d65c8b234b487bce03b
rubaiiyat <abirr1516@gmail.com>
2025-01-17 22:41:25 +0600

    setup header

--- 03-Auth-Integration/src/Components/Root/Root.jsx
+++ 03-Auth-Integration/src/Components/Root/Root.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Header from "../Header/Header";
+import { Outlet } from "react-router-dom";
 
 const Root = () => {
   return (
-    <div>
-      <h1>This is our root</h1>
+    <div className="text-center">
+      <Header></Header>
+      <Outlet></Outlet>
     </div>
   );
 };
--- 03-Auth-Integration/src/main.jsx
+++ 03-Auth-Integration/src/main.jsx
@@ -1,14 +1,31 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./Components/Root/Root.jsx";
+import Signup from "./Components/Signup/Signup.jsx";
+import Signin from "./Components/Signin/Signin.jsx";
+import Home from "./Components/Home/Home.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/signup",
+        element: <Signup></Signup>,
+      },
+      {
+        path: "/signin",
+        element: <Signin></Signin>,
+      },
+    ],
   },
 ]);
 
