dad9d69cf9ce6d652f9bb743d24a0855c506ee13
rubaiiyat <abirr1516@gmail.com>
2025-01-23 23:34:51 +0600

    Left side navigation

--- 04-News-Portal/src/Shared/LeftSideNav/LeftSideNav.jsx
+++ 04-News-Portal/src/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftSideNav = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("categories.json")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
   return (
     <div>
-      <h1>This is Left Side Nav</h1>
+      <h1 className="text-2xl font-bold text-white">All Category</h1>
+      {categories.map((category) => (
+        <Link
+          className="block ml-5 mt-5 text-lg text-info"
+          to={`/category/${category.id}`}
+        >
+          {category.name}
+        </Link>
+      ))}
     </div>
   );
 };
